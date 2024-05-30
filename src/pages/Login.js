1ec7f33766bef795d9cc71d3fe053997e88e3bf9
A2itE <a2itdummy@gmail.com>
2024-05-30 15:11:38 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -70,12 +70,12 @@ const Login = () => {
                         <span className='font-semibold text-xl'>Login to access your cart and orders.</span>
                     </div>
 
-                    <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
+                   <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
                         <div className="col-span-full">
                             <label className="block text-sm font-medium leading-6 text-gray-900">
                                 Email
                             </label>
-                            <div className="mt-2 ">
+                            <div className="mt-2">
                                 <input
                                     type='email'
                                     placeholder='enter email'
@@ -86,77 +86,32 @@ const Login = () => {
                                 />
                             </div>
                         </div>
-                        {/* <div className='grid'>
-                            <label>Email : </label>
-                            <div className='bg-slate-100 '>
-                                <input 
-                                    
-                                    className='w-full h-full outline-none bg-transparent'/>
-                            </div>
-                        </div> */}
-                        <div className="col-span-full ">
+                        <div className="col-span-full">
                             <label className="block text-sm font-medium leading-6 text-gray-900">
                                 Password
                             </label>
-                            <div className="mt-2 flex ">
+                            <div className="mt-2 relative">
                                 <input
                                     type={showPassword ? "text" : "password"}
                                     placeholder='enter password'
                                     value={data.password}
                                     name='password'
                                     onChange={handleOnChange}
-                                    className="p-2 relative w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6 bg-transparent"
+                                    className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
                                 />
-                                <div className='cursor-pointer absolute text-xl ml-[315px] mt-2' onClick={() => setShowPassword((preve) => !preve)}>
+                                <div className='cursor-pointer absolute right-3 top-1/2 transform -translate-y-1/2 text-xl' onClick={() => setShowPassword((prev) => !prev)}>
                                     <span>
-                                        {
-                                            showPassword ? (
-                                                <FaEyeSlash />
-                                            )
-                                                :
-                                                (
-                                                    <FaEye />
-                                                )
-                                        }
+                                        {showPassword ? <FaEyeSlash /> : <FaEye />}
                                     </span>
                                 </div>
                             </div>
                             <Link to={''} className='block w-fit ml-auto hover:underline hover:text-red-600'>
-                                Forgot password ?
+                                Forgot password?
                             </Link>
                         </div>
-
-                        {/* <div>
-                            <label className='text-sm font-medium leading-6 text-gray-900'>Password </label>
-                            <div className='bg-slate-100 p-2 flex'>
-                                <input
-                                    type={showPassword ? "text" : "password"}
-                                    placeholder='enter password'
-                                    value={data.password}
-                                    name='password'
-                                    onChange={handleOnChange}
-                                    className='w-full h-full outline-none bg-transparent' />
-                                <div className='cursor-pointer text-xl' onClick={() => setShowPassword((preve) => !preve)}>
-                                    <span>
-                                        {
-                                            showPassword ? (
-                                                <FaEyeSlash />
-                                            )
-                                                :
-                                                (
-                                                    <FaEye />
-                                                )
-                                        }
-                                    </span>
-                                </div>
-                            </div>
-                            <Link to={''} className='block w-fit ml-auto hover:underline hover:text-red-600'>
-                                Forgot password ?
-                            </Link>
-                        </div> */}
-
-                        <button className='bg-gray-900 hover:bg-gray-700 text-white px-6 py-2 w-full max-w-[150px] rounded-lg font-bold text-lg hover:scale-110 transition-all mx-auto block mt-6'>Login</button>
-
+                        <button className='bg-gray-900 hover:bg-gray-700 text-white px-6 py-2 w-full max-w-[150px] rounded-lg font-bold text-lg hover:scale-110 transition-all mx-auto block mt-6'>
+                            Login
+                        </button>
                     </form>
 
                     <p className='my-5'>Don't have account ? <Link to={"/sign-up"} className=' font-bold text-lg text-blue-700 hover:text-blue-700 hover:underline'>Sign up</Link></p>
